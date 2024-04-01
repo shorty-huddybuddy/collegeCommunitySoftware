72761b17f11918430fa7e548c5dde97746e985bf
SimpleSheep03 <sambhavsinha38@gmail.com>
2024-04-02 00:46:20 +0530

    added home page cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomePageCarousel from '../components/HomePageCarousel'
+import HomePageCards from '../components/HomePageCards'
 
 export default function Home() {
   return (
     <div>
         <HomePageCarousel/>
+        <HomePageCards/>
     </div>
   )
 }
